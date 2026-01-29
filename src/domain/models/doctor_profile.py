b5c5f58769e3f97516ffea7f6b79d9d2c190bbe8
tringuyen19 <tonyntri1903@gmail.com>
2026-01-29 21:46:17 +0700

    Delete src directory

--- src/domain/models/doctor_profile.py
+++
@@ -1,9 +0,0 @@
-class DoctorProfile:
-    def __init__(self, doctor_id: int, account_id: int, doctor_name: str, 
-                 specialization: str, license_number: str):
-        self.doctor_id = doctor_id
-        self.account_id = account_id
-        self.doctor_name = doctor_name
-        self.specialization = specialization
-        self.license_number = license_number
-
--- src/domain/models/role.py
+++
@@ -1,5 +0,0 @@
-class Role:
-    def __init__(self, role_id: int, role_name: str):
-        self.role_id = role_id
-        self.role_name = role_name
-
